d268a6b94fed1a60f27d052cb111fe413e7eec5a
Aidan Neel <aaidan5899@gmail.com>
2025-03-15 02:43:09 -0500

    Init

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+	import '../app.css';
+	let { children } = $props();
+</script>
+
+<svelte:head>
+	<title>aidan-neel</title>
+</svelte:head>
+
+<main class="w-screen h-screen fixed text-white flex items-start justify-center bg-neutral-900">
+	<div class="w-full md:w-2/3 lg:w-1/3 xl:w-1/2 2xl:w-1/4 h-screen py-16 px-8">
+		{@render children()}
+	</div>
+</main>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,33 @@
+<div class="flex flex-col gap-16">
+	<header>
+		<h1 class="font-medium">Aidan Neel</h1>
+		<h2 class="">Computer Scientist</h2>
+	</header>
+
+	<div class="flex flex-col gap-10">
+		<h1 class="font-medium">Projects</h1>
+		<div class="flex flex-col gap-6">
+			<a
+				href="https://ui.aidan-neel.com/"
+				class="flex hover:cursor-pointer items-start flex-col duration-200 hover:bg-neutral-800 rounded-md p-2 m-[-0.5rem]"
+			>
+				<h1 class="font-medium">Neel UI</h1>
+				<h2>Interactive and reusable UI components for Svelte</h2>
+			</a>
+			<a
+				href="https://github.com/aidan-neel/auth"
+				class="flex hover:cursor-pointer items-start flex-col duration-200 hover:bg-neutral-800 rounded-md p-2 m-[-0.5rem]"
+			>
+				<h1 class="font-medium">@aidan-neel/auth</h1>
+				<h2>TypeScript authentication library</h2>
+			</a>
+			<a
+				href="https://github.com/aidan-neel/pocketsvelte"
+				class="flex hover:cursor-pointer items-start flex-col duration-200 hover:bg-neutral-800 rounded-md p-2 m-[-0.5rem]"
+			>
+				<h1 class="font-medium">Pocketsvelte</h1>
+				<h2>Cybernetically enhanced Pocketbase apps</h2>
+			</a>
+		</div>
+	</div>
+</div>
